d609398d47ee582ce0a45d3cde65b727ed0c5733
fbiakbr <febiakbar19@gmail.com>
2022-02-03 20:09:39 +0700

    FxJikan v1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h3><strong>FxJikan</strong></h3>
+        <p><strong>FxJikan</strong> is a simple anime search engine.</p>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <form @submit.prevent="HandleSearch">
+                <div class="input-group mb-3">
+                  <input
+                    type="search"
+                    class="form-control"
+                    placeholder="Search for an Anime"
+                    v-model="search_query"
+                    required
+                  />
+                  <button
+                    class="btn btn-primary"
+                    @submit.prevent="HandleSearch"
+                  >
+                    Search
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Card v-for="anime in animelist" :key="anime.id" :anime="anime" />
+    </div>
+    <div class="row">
+      <div class="col-md-12 mt-3 mb-3">
+        <footer>
+          Build with ❤️ using Vue.js 3 and Jikan API V3 <br />
+          <a href="https://github.com/fbiakbr" target="_blank">Febiadi Wisnu Akbar</a>
+        </footer>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
+import { ref } from "vue";
 
 export default {
-  name: 'App',
+  setup() {
+    const search_query = ref("");
+    const animelist = ref([]);
+
+    const HandleSearch = async () => {
+      animelist.value = await fetch(
+        "https://api.jikan.moe/v3/search/anime?q=" + search_query.value
+      )
+        .then((res) => res.json())
+        .then((data) => data.results);
+
+      search_query.value = "";
+    };
+
+    return {
+      search_query,
+      animelist,
+      HandleSearch,
+    };
+  },
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.card {
+  text-align: left;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-md-3 mb-3">
+    <div class="container">
+      <div class="card">
+        <img :src="anime.image_url" class="card-img-top" />
+        <div class="card-body">
+          <h6 class="card-title">
+            <strong>{{ anime.title }}</strong>
+          </h6>
+          <p>Episode : {{ anime.episodes }}</p>
+          <p style="margin-top: -16px">Type : {{ anime.type }}</p>
+          <p style="margin-top: -16px">Score : {{ anime.score }}</p>
+          <p style="margin-top: -16px">Rating : {{ anime.rated }}</p>
+          <a class="btn btn-primary btn-sm" :href="anime.url" target="_blank">Read More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ["anime"],
+  name: "Card",
+};
+</script>
+
+<style></style>
